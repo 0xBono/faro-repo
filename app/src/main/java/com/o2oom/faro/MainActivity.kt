1f86c0a86b9d5e63a258c9695d6a93cf7f41f640
0xBono <0xbono@naver.com>
2020-01-19 04:09:17 +0900

    feat: Add fullscreen and NoactionBar

--- app/src/main/java/com/o2oom/faro/MainActivity.kt
+++ app/src/main/java/com/o2oom/faro/MainActivity.kt
@@ -2,11 +2,20 @@ package com.o2oom.faro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.pm.ActivityInfo
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.WindowManager
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
         setContentView(R.layout.activity_main)
     }
 }
