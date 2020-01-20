209ab7fabb38af61a562a02d2ec17a22a2a2c873
0xBono <0xbono@naver.com>
2020-01-20 20:19:45 +0900

    feat: Added time and date via SimpleDateFormat

--- app/src/main/java/com/o2oom/faro/MainActivity.kt
+++ app/src/main/java/com/o2oom/faro/MainActivity.kt
@@ -3,20 +3,27 @@ package com.o2oom.faro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.pm.ActivityInfo
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.view.WindowManager
-import android.widget.TextView
-import kotlin.time.ClockMark
+import com.o2oom.faro.R.layout
+import kotlinx.android.synthetic.main.activity_main.*
+import java.sql.Time
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+        setContentView(layout.activity_main)
 
-        setContentView(R.layout.activity_main)
+        //Local Time and Date
+
+        val localTime = Date()
+        val timeType = SimpleDateFormat("a hh:mm", Locale.KOREAN)
+        val dateType = SimpleDateFormat("MM월 dd일 EE", Locale.KOREAN)
+
+        LocalTime.text = timeType.format(localTime)
+        LocalDate.text = dateType.format(localTime)
     }
 }
