ed20c522b11ef60e9d91eec8d089b905fe920fac
0xBono <0xbono@naver.com>
2020-01-27 20:02:35 +0900

    chore(color): Hold until project refactoring

--- src/constants/ palette.js
+++ src/constants/ palette.js
@@ -1,8 +1,10 @@
-export const WHITE_COLOR = "#FFFFFF";
-export const GRAY_COLOR = "#C8C8C8";
-export const GREEN_COLOR = "#009688";
-export const YELLOW_COLOR = "#FFC107";
-export const BLUE_COLOR = "#4285F4";
-export const PURPLE_COLOR = "#6C63FF";
-export const ORANGE_COLOR = "#FF6347";
-export const DEFAULT_BACKGROUND_COLOR = "#272C30";
+// export const WHITE_COLOR = "#FFFFFF";
+// export const GRAY_COLOR = "#C8C8C8";
+// export const GREEN_COLOR = "#009688";
+// export const YELLOW_COLOR = "#FFC107";
+// export const BLUE_COLOR = "#4285F4";
+// export const PURPLE_COLOR = "#6C63FF";
+// export const ORANGE_COLOR = "#FF6347";
+// export const DEFAULT_BACKGROUND_COLOR = "#272C30"
+
+//Hold until project refactoring
