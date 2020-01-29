a5f9bfbc2de3a5208600084b2fba793146411402
0xBono <0xbono@naver.com>
2020-01-30 00:47:11 +0900

    chore(init): ionic-native init

--- src/screens/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SCREEN_DEVICE } from './SCREEN_DEVICE'
-export { default as SCREEN_DRIVING } from './SCREEN_DRIVING'
-export { default as SCREEN_MODE } from './SCREEN_MODE'
-export { default as SCREEN_RELAY } from './SCREEN_RELAY'
